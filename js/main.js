587eed2be673844927c51c388a089c8f4626287f
Luigi <luigismilzo@live.com>
2020-04-27 16:13:36 +0200

    Added requested functionality in JS

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 $(document).ready(function() {
 
+    // REFERENCES
+    var myApi = 'https://flynn.boolean.careers/exercises/api/random/int';
+    var square = $('.Grid-square');
 
 
+    // CLICK EVENT
+    square.click(function() {
+        var self = $(this);
+        
+        $.ajax({
+            url: myApi,
+            method: 'GET',
+            success: function(data) {
+                self.text(data.response);
 
-
-
-
-
-
-
-
-
-
-
-
+                if (data.response <= 5 ) {
+                    self.removeClass('Grid-square--yellow Grid-square--green')
+                    self.addClass('Grid-square--yellow');
+                } else {
+                    self.removeClass('Grid-square--yellow Grid-square--green')
+                    self.addClass('Grid-square--green');
+                }
+            },
+            error: function() {
+                console.error('Chiamata Ajax non riuscita');
+            }
+        });
+    });
 
 }); // <-- End ready
